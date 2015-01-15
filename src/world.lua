c1d71b579034b4f19df4f257a9887eb28934dba8
Nick Sprouffske <nick.sprouffske@sensus.com>
2015-01-14 23:39:58 -0700

    Established running of test via 'busted' and created some basic utility functions with tests

--- src/world.lua
+++ src/world.lua
@@ -1,7 +1,48 @@
-local world = {};
-
-function world:greet()
-  print("hello world");
+function default(...)
+  for i=1, select('#', ...) do
+    local arg = select(i, ...);
+    if (arg ~= nil) then
+      return arg;
+    end
+  end
 end
+
+local utils = {
+  __print = print,
+  __send = default(send, print),
+  default = default
+};
+
+function utils.join(separator, ...)
+  local argNum = select('#', ...);
+  local sep = utils.default(separator, ' ');
+  local assembly = '';
+  local isFirst = true;
+  for i=1, select('#', ...) do
+    local arg = select(i, ...);
+    local argType = type(arg);
+    if (argType == 'number' or argType == 'string') then
+      if (isFirst) then
+        assembly = arg;
+        isFirst = false;
+      else
+        assembly = assembly .. sep .. arg;
+      end
+    end
+  end
+  return assembly;
+end
+
+function utils.echo(...)
+  utils.__print(utils.join(' ', ...));
+end
+
+function utils.send(...)
+  utils.__send(utils.join(' ', ...));
+end
+
+local world = {
+  utils = utils
+};
 
 return world;
--- test/worldTest.lua
+++ test/worldTest.lua
@@ -1,5 +1,57 @@
-package.path = "../src/?.lua;" .. package.path;
+package.path = '../src/?.lua;src/?.lua;' .. package.path;
 local world = require("world");
 
-world.greet();
+local lastSend;
+local lastEcho;
+world.utils.__send = function(...)
+  lastSend = world.utils.join(' ', ...);
+end
+world.utils.__print = function(...)
+  lastEcho = world.utils.join(' ', ...);
+end
+
+describe('the core utilities', function()
+  local utils = world.utils;
+
+  after_each(function() 
+    lastSend = nil;
+    lastPrint = nil;
+  end);
+
+  it('provides utils.default', function()
+    assert.are_equal('value1', utils.default(nil, 'value1'));
+    assert.are_equal('value2', utils.default(nil, nil, 'value2', 'value3'));
+    assert.are_equal('value4', utils.default('value4', nil));
+    assert.is_nil(utils.default(nil, nil));
+    assert.are_equal(0, utils.default(0, 'value5'));
+    assert.are_equal(false, utils.default(false, 'value6'));
+    local func = function()end;
+    assert.are_equal(func, utils.default(func, 'value7'));
+  end);
+  it('provides utils.join', function()
+    assert.are_equal('2,three', utils.join(',', true, 2, 'three'));
+    assert.are_equal('2, three', utils.join(', ', true, 2, 'three'));
+    assert.are_equal('three', utils.join(',', true, nil, 'three'));
+    assert.are_equal('four', utils.join(',', 'four', function()end));
+    assert.are_equal('', utils.join(','));
+    assert.are_equal('', utils.join());
+    assert.are_equal('', utils.join(nil));
+  end);
+  it('provides utils.echo for commands to be delivered to the mud', function()
+    utils.echo('bs', 'beasty');
+    assert.are_equal('bs beasty', lastEcho);
+    utils.echo();
+    assert.are_equal('', lastEcho);
+    utils.echo(nil);
+    assert.are_equal('', lastEcho);
+  end);
+  it('provides utils.send for commands to be delivered to the mud', function()
+    utils.send('bs', 'beast');
+    assert.are_equal('bs beast', lastSend);
+    utils.send();
+    assert.are_equal('', lastSend);
+    utils.send(nil);
+    assert.are_equal('', lastSend);
+  end);
+end);
 
